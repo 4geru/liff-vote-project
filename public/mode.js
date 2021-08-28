a574a0d596ac837d04be3eb4e6b2294d49a07f96
4geru <westhouse51@gmail.com>
2021-08-28 15:32:39 +0900

    update answered mode

--- public/mode.js
+++ public/mode.js
@@ -36,3 +36,37 @@ const setVoteMode = () => {
     submit.addEventListener('click', submitVote)
     submit.innerText = '投票'
 }
+
+const setAnsweredMode = (votes, selectors) => {
+    document.getElementById('add-selector-btn').classList.add('d-none')
+    document.getElementById('change-mode-btn').classList.add('d-none')
+    // === 選択肢の表示 ===
+    const ul = document.getElementById('selector-list')
+    const lis = ul.getElementsByTagName('li')
+    selectors.forEach((selector, index) => {
+        // 投票結果を表示させる
+        if (votes.includes(`${selector.index}`)) {
+            lis[index].classList.add("active")
+        }
+        const newBadge = document.createElement("span")
+        newBadge.classList.add("badge", "bg-danger", "rounded-pill")
+        newBadge.innerHTML = selector.count
+        lis[index].appendChild(newBadge)
+    })
+
+    // === submitボタンの表示 ===
+    const submit = document.getElementById('submit-btn')
+    submit.innerText = 'シェア'
+    submit.removeEventListener('click', submitAddSelector)
+    submit.removeEventListener('click', submitVote)
+
+    submit.classList.add('d-none')
+    const liffClient = setLiffClient()
+    liffClient(async () => {
+        // 「LIFF が開ける && share target Picker が使える状態」 の場合に シェアボタンを表示する
+        if (liff.isApiAvailable('shareTargetPicker') && liff.isInClient()) {
+            submit.classList.remove('d-none')
+            submit.addEventListener('click', submitShare)
+        }
+    })
+}
